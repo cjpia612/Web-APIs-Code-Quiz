e8861e266b2848d379a6593fed06d1b18cbb2244
Cattleya Pia <cjpia612@gmail.com>
2020-04-22 22:11:04 -0700

    created pseudocode to guide

---
+++ script.js
@@ -0,0 +1,8 @@
+// On the homepage, there will be a greeting describing the quiz. When the Start button is clicked it will begin the quiz
+// Upon clicking the Start button, it will change to the first question displayed with 4 answer choices. 
+// If they choose the right answer, it moves on to the next question and displays Correct! underneath. 
+// If they choose the wrong answer, it moves on to the next question, but deducts 10 seconds from the timer. 
+// After all questions are answered, the timer stops and whatever number it stops on is the user's score.
+// If timer hits 0, the page will display Game Over. 
+// The page will display the user's score and an input field to write Initials down. 
+// the user's initials and score will be recorded to the highscore list. 
